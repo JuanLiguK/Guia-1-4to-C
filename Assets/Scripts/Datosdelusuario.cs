01f5c81029bdafdd61596efb4bea5c24c9c5a749
JuanLiguK <bl0016@est.ort.edu.ar>
2025-04-03 11:40:52 -0300

    cálculos

--- Assets/Scripts/Datosdelusuario.cs
+++ Assets/Scripts/Datosdelusuario.cs
@@ -13,7 +13,7 @@ public class Datosdelusuario : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Mi nombre es " + nombreUsuario + " tengo " + edadUsuario + " mido " + estaturaUsuario + " es " + esDonante);
+        Debug.Log("Mi nombre es " + nombreUsuario + " tengo " + edadUsuario + " mido " + estaturaUsuario + " metros y es " + esDonante + " que soy donante de organos");
     }
 
     // Update is called once per frame
